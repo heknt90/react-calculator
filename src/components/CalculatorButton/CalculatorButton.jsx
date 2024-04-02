fd4096931b168e8315f707afa6425e5c137904ba
Nikita Efremov <heknt90@mail.ru>
2024-04-02 20:28:39 +0300

    added view of &pi; symbol into CalculatorButton component

--- src/components/CalculatorButton/CalculatorButton.jsx
+++ src/components/CalculatorButton/CalculatorButton.jsx
@@ -1,6 +1,15 @@
+const htmlParse = (text) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(text, "text/html");
+  return doc.body.innerText;
+}
+
 export default function CalculatorButton({ label, clickHandler }) {
 
+  let text = label;
+  if (text === "PI") text = htmlParse("&pi;");
+
   return (
-    <button className="calculator__button" onClick={() => clickHandler(label)}>{label}</button>
+    <button className="calculator__button" onClick={() => clickHandler(label)}>{text}</button>
   )
 }
