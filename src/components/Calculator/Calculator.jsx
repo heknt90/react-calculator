156ae7a3cba305948a5648cf8fce4aaa8d970e9e
Nikita Efremov <heknt90@mail.ru>
2024-04-02 20:31:27 +0300

    changed state and click handlers for work with handing expression with eval function

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -2,112 +2,76 @@ import { useState } from "react"
 import CalculatorButton from "../CalculatorButton";
 import CalculatorDisplay from "./CalculatorDisplay/CalculatorDisplay";
 
-const initialOperation = {
-  operand1: 0,
-  operand2: 0,
-  operator: null,
-  result: null,
-};
-
-const operatorList = {
-  "x": multiplication,
-  "/": division,
-  "+": sum,
-  "-": difference,
-}
-
-function sum(operand1, operand2) {
-  return operand1 + operand2;
-}
-
-function difference(operand1, operand2) {
-  return operand1 - operand2;
-}
-
-function multiplication(operand1, operand2) {
-  return operand1 * operand2;
-}
-
-function division(operand1, operand2) {
-  return operand1 / operand2;
-}
-
 export default function Calculator() {
-  const [operation, setOperation] = useState(initialOperation);
+  const [expression, setExpression] = useState("");
+  const [result, setResult] = useState(null);
+  const [history, setHistory] = useState("");
 
-  function concat(prev, digit) {
-    return parseFloat("" + prev + digit)
+  function addCharToExpressionHandler(char) {
+    if (!result) {
+      if (char === "x") char = "*";
+      if (char === ",") char = ".";
+      if (char === "PI") char = Math.PI;
+      setExpression(prev => prev + char);
+    }
   }
 
-  function setOperand(value, operandName) {
-    setOperation(oldOperation => {
-      const newOperation = Object.assign({}, oldOperation);
-      newOperation[operandName] = concat(newOperation[operandName], value);
-      return newOperation;
-    });
+  function changeSignHandler() {
+
+  }
+
+  function deleteLastCharHandler() {
+    if (expression !== "") {
+      setExpression(prev => prev.slice(0, -1));
+    }
   }
 
   function clearButtonHandler() {
-    setOperation(prev => initialOperation);
+    setExpression(prev => "");
+    setResult(prev => null);
   }
 
   function resultButtonHandler() {
-    const func = operatorList[operation.operator];
-    setOperation(prev => {
-      const result = func(prev.operand1, prev.operand2);
-      return ({ ...prev, result });
-    });
-  }
-
-  function addOperandDigitButtonHandler(digit) {
-    if (!operation.result) {
-      let operandName = "operand1";
-      if (!!operation.operator) {
-        operandName = "operand2";
+    try {
+      if (expression !== "" && eval(expression)) {
+        setResult(prev => eval(expression));
       }
-      setOperand(digit, operandName);
-    }
-  }
-
-  function setOperatorButtonHandler(operator) {
-    if (!operation.result) {
-      setOperation(prev => {
-        const newOperation = Object.assign({}, prev);
-        newOperation.operator = operator;
-        return newOperation;
-      });
-    }
+    } catch (e) { }
   }
 
   return (
     <div>
       <div className="calculator">
-        <CalculatorDisplay operation={operation} />
+        <CalculatorDisplay expression={expression} result={result} history={history} />
 
         <div className="calculator__controls">
-          <CalculatorButton label="(" />
-          <CalculatorButton label=")" />
+          <div style={{ gridColumn: "2" }}></div>
           <CalculatorButton label="C" clickHandler={clearButtonHandler} />
-          <CalculatorButton label="/" clickHandler={setOperatorButtonHandler} />
+          <CalculatorButton label="Del" clickHandler={deleteLastCharHandler} />
 
-          <CalculatorButton label="7" clickHandler={addOperandDigitButtonHandler} />
-          <CalculatorButton label="8" clickHandler={addOperandDigitButtonHandler} />
-          <CalculatorButton label="9" clickHandler={addOperandDigitButtonHandler} />
-          <CalculatorButton label="x" clickHandler={setOperatorButtonHandler} />
+          <CalculatorButton label="(" clickHandler={addCharToExpressionHandler} />
+          <CalculatorButton label=")" clickHandler={addCharToExpressionHandler} />
+          <CalculatorButton label="PI" clickHandler={addCharToExpressionHandler} />
+          <CalculatorButton label="/" clickHandler={addCharToExpressionHandler} />
 
-          <CalculatorButton label="4" clickHandler={addOperandDigitButtonHandler} />
-          <CalculatorButton label="5" clickHandler={addOperandDigitButtonHandler} />
-          <CalculatorButton label="6" clickHandler={addOperandDigitButtonHandler} />
-          <CalculatorButton label="-" clickHandler={setOperatorButtonHandler} />
+          <CalculatorButton label="7" clickHandler={addCharToExpressionHandler} />
+          <CalculatorButton label="8" clickHandler={addCharToExpressionHandler} />
+          <CalculatorButton label="9" clickHandler={addCharToExpressionHandler} />
+          <CalculatorButton label="x" clickHandler={addCharToExpressionHandler} />
 
-          <CalculatorButton label="1" clickHandler={addOperandDigitButtonHandler} />
-          <CalculatorButton label="2" clickHandler={addOperandDigitButtonHandler} />
-          <CalculatorButton label="3" clickHandler={addOperandDigitButtonHandler} />
-          <CalculatorButton label="+" clickHandler={setOperatorButtonHandler} />
+          <CalculatorButton label="4" clickHandler={addCharToExpressionHandler} />
+          <CalculatorButton label="5" clickHandler={addCharToExpressionHandler} />
+          <CalculatorButton label="6" clickHandler={addCharToExpressionHandler} />
+          <CalculatorButton label="-" clickHandler={addCharToExpressionHandler} />
 
-          <CalculatorButton label="+/-" />
-          <CalculatorButton label="0" clickHandler={addOperandDigitButtonHandler} />
-          <CalculatorButton label="," clickHandler={addOperandDigitButtonHandler} />
+          <CalculatorButton label="1" clickHandler={addCharToExpressionHandler} />
+          <CalculatorButton label="2" clickHandler={addCharToExpressionHandler} />
+          <CalculatorButton label="3" clickHandler={addCharToExpressionHandler} />
+          <CalculatorButton label="+" clickHandler={addCharToExpressionHandler} />
+
+          <CalculatorButton label="+/-" clickHandler={changeSignHandler} />
+          <CalculatorButton label="0" clickHandler={addCharToExpressionHandler} />
+          <CalculatorButton label="," clickHandler={addCharToExpressionHandler} />
           <CalculatorButton label="=" clickHandler={resultButtonHandler} />
         </div>
       </div>
